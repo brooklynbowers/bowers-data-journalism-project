ea4652133990149ccdc57a4a2790faab14bbe9f0
brooklyn bowers <151276772+brooklynbowers@users.noreply.github.com>
2024-05-22 23:10:18 -0400

    Update exploratory_data_analysis.py

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -1,1 +1,10 @@
+# Exploratory Data Analysis
+
+# tasks:
+# 1. create a line plot that shows the overall hourly wage trend over time
+# 2. comparing the overall hourly wage trend (total employees) against health and 
+# engineering professionals
+
+######################
+
 
